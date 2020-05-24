d735f21f6da6d1f8de431cf8c707acbb66e188e6
milyyy <milysong@163.com>
2020-05-24 21:07:47 +0800

    全局引入

--- examples/main.js
+++ examples/main.js
@@ -3,9 +3,15 @@ import router from "./router"
 import store from "./store"
 import App from "./App.vue"
 
+// 全局引入
 import milyUI from "./../packages"
-
 Vue.use(milyUI)
+
+// 按需引入
+// import {Test} from "./../packages/test"
+// import {Alert} from "./../packages/alert"
+// Vue.use(Test)
+// Vue.use(Alert)
 
 Vue.config.productionTip = false
 
