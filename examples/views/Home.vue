b1b62c2dcdbd4f495c4b9355f79cf0d966afa1b4
milyyy <milysong@163.com>
2020-05-27 17:35:54 +0800

    button完善

--- examples/views/Home.vue
+++ examples/views/Home.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>
-    测试<m-test></m-test>     <br>
-    弹窗<m-alert></m-alert>   <br>
-    按钮<m-button type="primary">默认</m-button> <br>
+  <div class="button">
+    按钮<br>
+    <m-button type="primary">主要</m-button> 
+    <m-button type="success">成功</m-button>
+    <m-button type="warning">警告</m-button>
+    <m-button type="danger">危险</m-button>
+    <m-button type="info">信息</m-button> <br>
+    <m-button type="primary" disabled>主要</m-button>
+    <m-button type="success" disabled>成功</m-button>
+    <m-button type="warning" disabled>警告</m-button>
+    <m-button type="danger" disabled>危险</m-button>
+    <m-button type="info" disabled>信息</m-button> <br>
+    圆角<br>
+    <m-button type="info" size="small" circle>圆</m-button>
+    <m-button type="info" size="small" disabled circle>圆</m-button>
+    <m-button type="info" round>圆角</m-button>
+    <m-button type="info" disabled round>圆角</m-button> <br>
+    尺寸<br>
+    <m-button @click="handleClick">默认</m-button>  -- | -- 
+    <m-button type="primary" size="mini">迷你</m-button>
+    <m-button type="success" size="small">小型</m-button>
+    <m-button type="warning" size="medium">中等</m-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  methods: {
+    handleClick() {
+      alert('this is a test')
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .button {
+    padding: 20px;
+  }
+</style>
--- packages/button/src/main.vue
+++ packages/button/src/main.vue
@@ -11,24 +11,28 @@
  * 插槽
 --> 
 <template>
-  <div>
-    <button
-      class="m-button"
-      :disabled="ButtonDisabled || loading"
-      :class="[
-        type ? 'm-button--' + type: '',
-        {
-          'is-disabled': ButtonDisabled,
-          'is-loading': loading
-        }
-      ]"
-    >
-      <!-- 有slot才显示 -->
-      <span v-if="$slots.default">
-        <slot></slot>
-      </span>
-    </button>
-  </div>
+  <button
+    class="m-button"
+    :disabled="ButtonDisabled || loading"
+    :type="nativeType"
+    :autofocus="autofocus"
+    @click="handleClick"
+    :class="[
+      type ? 'm-button--' + type: '',
+      size ? 'm-button--' + size: '',
+      {
+        'is-disabled': ButtonDisabled,
+        'is-loading': loading,
+        'is-round': round,
+        'is-circle': circle
+      }
+    ]"
+  >
+    <!-- 有slot才显示 loading暂未设计 -->
+    <span v-if="$slots.default">
+      <slot></slot>
+    </span>
+  </button>
 </template>
 
 <script>
@@ -38,6 +42,14 @@ export default {
     type: {
       type: String,
       default: ""
+    },
+    size: {
+      type: String,
+      default: "small"
+    },
+    nativeType: { // 原生button的type属性，值为button, submit或rest
+      type: String,
+      default: 'button'
     },
     disabled: {  // 为true时，既可以设置disabled为true，又可以加载禁用时的样式
       type: Boolean,
@@ -49,7 +61,8 @@ export default {
     }, 
     plain: Boolean,
     round: Boolean,
-    circle: Boolean
+    circle: Boolean,
+    autofocus: Boolean
   },
   computed: {
     ButtonDisabled() {
